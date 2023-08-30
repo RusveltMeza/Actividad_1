1e1971c8d46b7bedff376900662a93bfdd54782c
RusveltMeza <101429716+RusveltMeza@users.noreply.github.com>
2023-08-30 13:42:50 -0500

    Update POOER17.java

--- POOER17.java
+++ POOER17.java
@@ -10,16 +10,18 @@ import java.util.Scanner;
  *
  * @author rusve
  */
+
 public class POOER17 {
     public static void main(String[] args) {
         double radio_c, area_c, longitud_c;
         Scanner entrada = new Scanner(System.in);
         System.out.println("Ingrese el valor del radio: ");
         radio_c = entrada.nextDouble();
-        area_c = Math.PI*(radio_c*radio_c);
-        longitud_c = 2*radio_c*Math.PI;
-        
+        area_c = Math.PI * (radio_c * radio_c);
+        longitud_c = 2 * radio_c * Math.PI;
+
         System.out.println("El area del circulo es: " + area_c);
         System.out.println("La longitud del circulo es: " + longitud_c);
+        entrada.close();
     }
 }
